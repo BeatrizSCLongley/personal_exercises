10ce360b15d7cda7ba45cd3dc35583ad146d1079
BeatrizSCLongley <beatrizsousacosta@gmail.com>
2024-09-06 09:43:19 +1200

    fix typo on ingridients

--- cooking_code/01-Calculate-baking/lib/calculate_baking.rb
+++ cooking_code/01-Calculate-baking/lib/calculate_baking.rb
@@ -1,9 +1,9 @@
-# method that takes a recipe array of ingridients and their amounts
-# and a pantry array of ingridients available with their amounts
+# method that takes a recipe array of ingredients and their amounts
+# and a pantry array of ingredients available with their amounts
 # return an integer of how many 'cakes' you can bake with the given 
 # available & necessary amounts 
 
-# check if all ingridients necessary are available in pantry
-# check that all ingridient amounts in pantry are > than in recipe
+# check if all ingredients necessary are available in pantry
+# check that all ingredient amounts in pantry are > than in recipe
 # divide all the amounts from pantry with recipe to calculate ratios
 # and return the lowest number 
