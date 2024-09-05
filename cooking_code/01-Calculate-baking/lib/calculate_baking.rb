b07bd6569ed8ecc9b4f667eb12b22cc4ba956d20
BeatrizSCLongley <beatrizsousacosta@gmail.com>
2024-09-06 10:16:58 +1200

    method to check necessary amounts

--- cooking_code/01-Calculate-baking/lib/calculate_baking.rb
+++ cooking_code/01-Calculate-baking/lib/calculate_baking.rb
@@ -8,8 +8,6 @@
 # divide all the amounts from pantry with recipe to calculate ratios
 # and return the lowest number 
 
-# don't worry about converting amounts for each ingredient
-
 class CalculateBaking
   def initialize(recipe, pantry)
     @recipe = recipe
@@ -28,15 +26,28 @@ class CalculateBaking
   
   def have_necessary_amounts?
     # if key is same compare value >= v
+    count = 0
+    @pantry.each_pair do |key, value|
+      @recipe.each_pair do |ingredient, amount|
+        if key == ingredient
+          count += 1 if value >= amount
+        end
+      end
+    end
+
+    count == @number_necessary_ingredients
   end
 
-  def how_many_cakes?
+  def number_of_cakes
+    # "You are missing some ingredients" unless have_all_ingredients?
+    # "You don't have enough from some of the ingredients" unless have_necessary_amounts?
   end
 end
 
 recipe = { "flour" => 300, "sugar" => 200, "eggs" => 2 }
-pantry = { "flour" => 300, "sugar" => 200, "eggs" => 5, "milk" => 1 }
+pantry = { "flour" => 500, "sugar" => 200, "eggs" => 3, "milk" => 1 }
 
 calculate_baking = CalculateBaking.new(recipe, pantry)
 
 puts calculate_baking.have_all_ingredients?
+puts calculate_baking.have_necessary_amounts?
