8f05bca67f5fa229f7aeb9623873ad236bb626ec
BeatrizSCLongley <beatrizsousacosta@gmail.com>
2024-09-06 12:11:02 +1200

    spec files

--- cooking_code/01-Calculate-baking/lib/calculate_baking.rb
+++ cooking_code/01-Calculate-baking/lib/calculate_baking.rb
@@ -1,26 +1,17 @@
+# frozen_string_literal: true
+
 # program that takes a recipe hash of ingredients and their amounts
 # and a pantry hash of ingredients available with their amounts
-# return an integer of how many 'cakes' you can bake with the given 
-# available & necessary amounts 
-
-# check if all ingredients necessary are available in pantry
-# check that all ingredient amounts in pantry are > than in recipe
-# divide all the amounts from pantry with recipe to calculate ratios
-# and return the lowest number 
-
-# HOW TO IMPROVE THE CODE / WHAT TO DO NEXT
-# - do we need all these methods?
-# - should we remove unecessary elements from pantry?
-# - should we save the ingredient(s) missing so we can provide a better sring
-
+# return an integer of how many 'cakes' you can bake with the given
+# available & necessary amounts
 class CalculateBaking
   def initialize(recipe, pantry)
     @recipe = recipe
     @pantry = pantry
     @number_necessary_ingredients = recipe.size
   end
-  
-  def have_all_ingredients?
+
+  def all_ingredients?
     count = 0
     @pantry.each_key do |key|
       @recipe.each_key { |k| count += 1 if key == k }
@@ -28,8 +19,8 @@ class CalculateBaking
 
     count == @number_necessary_ingredients
   end
-  
-  def have_necessary_amounts?
+
+  def necessary_amounts?
     count = 0
     @pantry.each_pair do |key, value|
       @recipe.each_pair do |ingredient, amount|
@@ -43,7 +34,9 @@ class CalculateBaking
   end
 
   def number_of_cakes
-    "You are missing some ingredients" unless have_all_ingredients? && have_necessary_amounts?
+    return 'Your pantry is empty' if @pantry.empty?
+
+    return 'You are missing some ingredients' unless all_ingredients? && necessary_amounts?
 
     ratios = calculate_ratios
     ratios.min
@@ -55,9 +48,7 @@ class CalculateBaking
     ratios = []
     @pantry.each_pair do |key, value|
       @recipe.each_pair do |ingredient, amount|
-        if key == ingredient
-          ratios << value / amount
-        end
+        ratios << value / amount if key == ingredient
       end
     end
 
@@ -65,11 +56,7 @@ class CalculateBaking
   end
 end
 
-recipe = { "flour" => 300, "sugar" => 200, "eggs" => 2 }
-pantry = { "flour" => 2000, "sugar" => 400, "eggs" => 5, "milk" => 1 }
-
-calculate_baking = CalculateBaking.new(recipe, pantry)
-
-puts calculate_baking.have_all_ingredients?
-puts calculate_baking.have_necessary_amounts?
-puts calculate_baking.number_of_cakes
+# HOW TO IMPROVE THE CODE / WHAT TO DO NEXT
+# - do we need all these methods?
+# - should we remove unecessary elements from pantry?
+# - should we save the ingredient(s) missing so we can provide a better sring
---
+++ cooking_code/01-Calculate-baking/spec/calculate_baking_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'calculate_baking'
+
+describe 'CalculateBaking' do
+  let(:recipe) { { 'flour' => 300, 'sugar' => 200, 'eggs' => 2 } }
+  let(:full_pantry) { { 'flour' => 2000, 'sugar' => 400, 'eggs' => 5, 'milk' => 1 } }
+  let(:low_pantry) { { 'flour' => 100, 'sugar' => 100, 'eggs' => 1 } }
+  let(:empty_pantry) { {} }
+  let(:pantry_is_empty_message) { 'Your pantry is empty' }
+  let(:missing_ingredient_message) { 'You are missing some ingredients' }
+
+  context 'When we have enough ingredients in our pantry' do
+    let(:calculate_baking) { CalculateBaking.new(recipe, full_pantry) }
+
+    describe '#have_all_ingredients?' do
+      it 'returns true' do
+        expect(calculate_baking.all_ingredients?).to eq true
+      end
+    end
+
+    describe '#have_necessary_amounts?' do
+      it 'returns true' do
+        expect(calculate_baking.necessary_amounts?).to eq true
+      end
+    end
+
+    describe '#number_of_cakes' do
+      it 'returns the number of cakes the user can bake with the given ingredients' do
+        expect(calculate_baking.number_of_cakes).to eq 2
+      end
+    end
+  end
+
+  context 'When we don\'t have enough ingredients in our pantry' do
+    let(:calculate_baking) { CalculateBaking.new(recipe, low_pantry) }
+
+    describe '#have_all_ingredients?' do
+      it 'returns true' do
+        expect(calculate_baking.all_ingredients?).to eq true
+      end
+    end
+
+    describe '#have_necessary_amounts?' do
+      it 'returns false' do
+        expect(calculate_baking.necessary_amounts?).to eq false
+      end
+    end
+
+    describe '#number_of_cakes' do
+      it 'returns an error message' do
+        expect(calculate_baking.number_of_cakes).to eq missing_ingredient_message
+      end
+    end
+  end
+
+  context 'When we don\'t have any ingredients' do
+    let(:calculate_baking) { CalculateBaking.new(recipe, empty_pantry) }
+
+    describe '#have_all_ingredients?' do
+      it 'returns true' do
+        expect(calculate_baking.all_ingredients?).to eq false
+      end
+    end
+
+    describe '#have_necessary_amounts?' do
+      it 'returns true' do
+        expect(calculate_baking.necessary_amounts?).to eq false
+      end
+    end
+
+    describe '#number_of_cakes' do
+      it 'returns an error message' do
+        expect(calculate_baking.number_of_cakes).to eq pantry_is_empty_message
+      end
+    end
+  end
+end
